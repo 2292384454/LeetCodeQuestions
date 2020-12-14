b52b63bb39cf2667b9fc1a2eb7f52e44f51eb6c2
KevinHwang <2292384454@qq.com>
2020-12-14 12:15:07 +0800

    Kevin's Leetcode solution

---
+++ src/leetcode/editor/cn/P1513NumberOfSubstringsWithOnly1s.java
@@ -0,0 +1,82 @@
+//给你一个二进制字符串 s（仅由 '0' 和 '1' 组成的字符串）。 
+//
+// 返回所有字符都为 1 的子字符串的数目。 
+//
+// 由于答案可能很大，请你将它对 10^9 + 7 取模后返回。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：s = "0110111"
+//输出：9
+//解释：共有 9 个子字符串仅由 '1' 组成
+//"1" -> 5 次
+//"11" -> 3 次
+//"111" -> 1 次 
+//
+// 示例 2： 
+//
+// 输入：s = "101"
+//输出：2
+//解释：子字符串 "1" 在 s 中共出现 2 次
+// 
+//
+// 示例 3： 
+//
+// 输入：s = "111111"
+//输出：21
+//解释：每个子字符串都仅由 '1' 组成
+// 
+//
+// 示例 4： 
+//
+// 输入：s = "000"
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// s[i] == '0' 或 s[i] == '1' 
+// 1 <= s.length <= 10^5 
+// 
+// Related Topics 数学 字符串 
+// 👍 10 👎 0
+
+package leetcode.editor.cn;
+
+//Java：仅含 1 的子串数
+public class P1513NumberOfSubstringsWithOnly1s {
+    public static void main(String[] args) {
+        Solution solution = new P1513NumberOfSubstringsWithOnly1s().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numSub(String s) {
+            boolean start = false;
+            long result = 0, NUM = 1000000007, count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '1') {
+                    start = true;
+                    count++;
+                }
+                if (s.charAt(i) == '0' || (i + 1) == s.length()) {
+                    if (start == true) {
+                        count = (count + 1) * count / 2;
+                        result += count;
+                    }
+                    start = false;
+                    count = 0;
+                }
+            }
+            return (int) (result % NUM);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
